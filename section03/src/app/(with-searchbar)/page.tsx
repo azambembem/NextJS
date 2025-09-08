4c837ce0acd0a1d593f311354efc7684ff627f98
azambembem <azamjan980713@gmail.com>
2025-09-08 22:08:30 +0900

    env

--- section03/src/app/(with-searchbar)/page.tsx
+++ section03/src/app/(with-searchbar)/page.tsx
@@ -4,9 +4,11 @@ import books from "@/mock/books.json";
 import { BookData } from "@/types";
 
 async function AllBooks() {
-  const response = await fetch("http://localhost:12345/bookds");
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
   if (!response.ok) {
-    return <div>데이터를 찾을 수 없습니다</div>;
+    return <div>데이터를 찾을 수 없습니다...</div>;
   }
   const allBooks: BookData[] = await response.json();
 
@@ -19,16 +21,30 @@ async function AllBooks() {
   );
 }
 
-function RecoBooks() {}
+async function RecoBooks() {
+  const response = await fetch("http://localhost:12345/book/random");
+  if (!response.ok) {
+    return <div>데이터를 찾을 수 없습니다...</div>;
+  }
+  const recoBooks: BookData[] = await response.json();
+  return (
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        {/* {books.map((book) => (
+          <BookItem key={book.id} {...book} /> // yani buni vazifasini pastdagi RecoBooks bajariyapti
+        ))} */}
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
